389f1361db6543abe0a24d0389ff9958b35a0dca
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-10-24 20:35:47 -0300

    Update AbstractShape.java

--- src/interfaces/herancas/abstrata/model/entities/AbstractShape.java
+++ src/interfaces/herancas/abstrata/model/entities/AbstractShape.java
@@ -1,23 +1,20 @@
 package interfaces.herancas.abstrata.model.entities;
+
+
 import interfaces.herancas.abstrata.model.enums.Color;
 
-// Classe abstrata AbstractShape que implementa a interface Shape
-// Ela serve como uma base para outras formas geométricas que compartilharão características comuns
 public abstract class AbstractShape implements Shape {
 
     private Color color;
 
-    // Construtor que inicializa a cor da forma
     public AbstractShape(Color color) {
         this.color = color;
     }
 
-    // Método getter que retorna a cor da forma
     public Color getColor() {
         return color;
     }
 
-    // Método setter que permite alterar a cor da forma
     public void setColor(Color color) {
         this.color = color;
     }
